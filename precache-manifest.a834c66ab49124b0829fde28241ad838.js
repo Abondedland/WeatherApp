74ec4798c55991d241828d0e2084028e3de5e0e0
Abondedland <AbondedLand>
2023-12-23 11:53:34 +0300

    Updates

---
+++ precache-manifest.a834c66ab49124b0829fde28241ad838.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "452486268365a19551db46d0fa4131f3",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "7072528853dc0c0c5fe4",
+    "url": "/WeatherApp/static/css/main.e0c0522a.chunk.css"
+  },
+  {
+    "revision": "47692ce3783c3c0d7d84",
+    "url": "/WeatherApp/static/js/2.21edb28f.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/WeatherApp/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "7072528853dc0c0c5fe4",
+    "url": "/WeatherApp/static/js/main.6ba92ded.chunk.js"
+  },
+  {
+    "revision": "96b9f29eea0185fc7d82",
+    "url": "/WeatherApp/static/js/runtime~main.e827384c.js"
+  }
+]);
