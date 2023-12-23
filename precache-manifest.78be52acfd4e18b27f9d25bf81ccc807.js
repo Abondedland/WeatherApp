3a7b613e7bb9eb7cd1ff1980cc4445a2cf3d0c60
Abondedland <AbondedLand>
2023-12-23 12:53:32 +0300

    Updates

---
+++ precache-manifest.78be52acfd4e18b27f9d25bf81ccc807.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d067afb48d96529fd0819d9ceb20e3ce",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "24e4f3521947e5828899",
+    "url": "/WeatherApp/static/css/main.e0c0522a.chunk.css"
+  },
+  {
+    "revision": "8f5b971ea337e491303b",
+    "url": "/WeatherApp/static/js/2.49a0c9e9.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/WeatherApp/static/js/2.49a0c9e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07ecfbc643984d6679bf",
+    "url": "/WeatherApp/static/js/3.5480e1f6.chunk.js"
+  },
+  {
+    "revision": "24e4f3521947e5828899",
+    "url": "/WeatherApp/static/js/main.9787e515.chunk.js"
+  },
+  {
+    "revision": "0fffe77a5954de4695ac",
+    "url": "/WeatherApp/static/js/runtime-main.8ba1e2bc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/WeatherApp/precache-manifest.a834c66ab49124b0829fde28241ad838.js"
+  "/WeatherApp/precache-manifest.78be52acfd4e18b27f9d25bf81ccc807.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/WeatherApp/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
